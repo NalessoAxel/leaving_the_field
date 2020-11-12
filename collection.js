bb9d0ef13896178107ff76c36fbaa64e712a1062
NalessoAxel <axelnalesso@gmail.com>
2020-11-12 09:25:44 +0100

    ajout dark mode

---
+++ collection.js
@@ -0,0 +1,101 @@
+
+
+
+
+
+
+const collection = [
+    {
+        title: 'Chernobyl',
+        author: 'Craig Mazin',
+        category: ['Drame', 'Historique'],
+        link : 'https://www.youtube.com/watch?v=s9APLXM9Ei8&feature=emb_title"',
+        description: '26 avril 1986, l\'histoire vraie de la pire catastrophe causée par l\'homme et de ceux qui ont sacrifié leur vie pour sauver l\'Europe du drame.'
+        
+
+    },
+    {
+        title: 'Dark',
+        author: ['Baran bo Odar','Jantje Friese'],
+        category: ['Policier', 'Science-fiction'],
+        link : 'https://www.youtube.com/watch?v=qJpIHpZSkXo',
+        description: 'Un enfant disparu lance quatre familles dans une quête éperdue pour trouver des réponses. La chasse au coupable fait émerger les péchés et les secrets d\'une petite ville.'
+        
+        
+
+    },
+    {
+        title: 'MindHunter',
+        author: 'Joe Penhall',
+        category: ['Drame', 'Thriller'],
+        link : 'https://www.youtube.com/watch?v=YnLQzzJMnV8&feature=emb_title',
+        description: 'Comment anticiper la folie quand on ignore comment fonctionnent les fous? Deux agents du FBI imaginent une enquête aux méthodes révolutionnaires et se lancent dans une véritable odyssée pour obtenir des réponses.'
+        
+
+    },
+    {
+        title: 'Mr.Robot',
+        author: 'Sam Esmail',
+        category: ['Drame', 'Thriller'],
+        link : 'https://www.youtube.com/watch?v=uml6bz9ygxA&feature=emb_title',
+        description: 'Elliot est un jeune programmeur antisocial qui souffre d\'un trouble du comportement. Il est recruté par un anarchiste mystérieux, qui se fait appeler Mr. Robot.'
+        
+
+    },
+    {
+        title: 'Twin Peaks',
+        author: 'David Lynch',
+        category: ['Drame', 'Thriller'],
+        link : 'https://www.youtube.com/watch?v=2rmC9M1yla0',
+        description: 'Un meurtre a été commis à Twin Peaks, une petite bourgade en apparence tranquille.La jeune Laura Palmer est retrouvée morte nue au bord d\'un lac.'
+        
+
+    },
+    {
+        title: 'Westworld',
+        author: ['Jonathan Nolan', 'Lisa Joy'],
+        category: ['Thriller', 'Science-fiction'],
+        link : 'https://www.youtube.com/watch?v=9BqKiZhEFFw',
+        description: ' Westworld, un parc d\'attractions dernier cri, les visiteurs paient des fortunes pour revivre le frisson de la conquête de l\'Ouest.'
+        
+
+    },
+    {
+        title: 'The Terror',
+        author: 'Joe Penhall',
+        category: ['Drame', 'Thriller'],
+        link : 'https://www.youtube.com/watch?v=pjq7Gl_hhPY',
+        description: 'En 1847, la Marine Royale britannique entreprend une périlleuse mission : découvrir le passage du Nord. Forcé de prendre le commandement, le capitaine Francis Crozier devra gérer le froid glacial, les menaces de mutinerie, le manque de nourriture ainsi qu\'une menace bien pire encore...'
+        
+
+    },
+    {
+        title: 'True detective',
+        author: 'Nic Pizzolatto',
+        category: ['Drame', 'Policier'],
+        link : 'https://www.youtube.com/watch?v=D-kWun2a7oU&feature=emb_title',
+        description: 'A chaque saison, son histoire. True Detective nous embarque dans des récits policiers mêlant mysticisme, réflexions philosophiques et personnages torturés'
+        
+
+    },
+    {
+        title: 'Fargo',
+        author: 'Noah Hawley',
+        category: ['Comedy', 'Policier'],
+        link : 'https://www.youtube.com/watch?v=gKs8DzjPDMU&feature=emb_title',
+        description: 'Le quotidien des habitants d\'une petite ville du Minnesota se retrouve bouleversé suite au passage d\'un tueur à gages.'
+        
+
+    },
+    {
+        title: 'The Mandalorian',
+        author: 'Jon Favreau',
+        category: ['Aventure', 'Science-fiction'],
+        link : 'https://www.youtube.com/watch?v=O6VKY1UkBak&feature=emb_title',
+        description: 'Le Mandalorien se déroule après la chute de l\'Empire et juste avant l\'émergence de Premier Ordre et raconte les voyages dans les contrées les plus éloignées de la Galaxie d\'un tireur solitaire loin de l\’autorité de la Nouvelle République.'
+        
+
+    },
+];
+  
+
